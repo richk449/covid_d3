975a6674aec6076294647c01e9775cc9a411d63d
Rich Kowalczyk <rich.kowalczyk@gmail.com>
2020-04-02 20:48:44 -0700

    added plotting booleans to python

--- plot_covid_x1.py
+++ plot_covid_x1.py
@@ -7,6 +7,9 @@ legendFontsize = 6
 thresholdConfirmedCountry = 5000
 normValueConfirmedCountry = 1000
 
+makePlots = False
+showPlots = False
+
 #===============================================================
 # load in data file
 #===============================================================
@@ -41,36 +44,39 @@ for name in regionDict:
 #===============================================================
 # plot
 #===============================================================
-figCountry = pl.figure(figsize=figsize)
-axCountry = figCountry.add_subplot(1,2,1)
-axCountryNorm = figCountry.add_subplot(1,2,2)
+if(makePlots):
+    figCountry = pl.figure(figsize=figsize)
+    axCountry = figCountry.add_subplot(1,2,1)
+    axCountryNorm = figCountry.add_subplot(1,2,2)
 
-maxNormDays = 0.0
-maxVal = 0.0
-for name in countryDict:
-    if(max(countryDict[name]) > thresholdConfirmedCountry):
-        days = list(range(len(countryDict[name])))
-        axCountry.semilogy(countryDict[name], label=name)
-        
-        indexNorm = np.argmin(abs(countryDict[name] - normValueConfirmedCountry))
-        normDays = days - indexNorm
-        axCountryNorm.semilogy(normDays, countryDict[name], label=name)
+    maxNormDays = 0.0
+    maxVal = 0.0
+    for name in countryDict:
+        if(max(countryDict[name]) > thresholdConfirmedCountry):
+            days = list(range(len(countryDict[name])))
+            axCountry.semilogy(countryDict[name], label=name)
+            
+            indexNorm = np.argmin(abs(countryDict[name] - normValueConfirmedCountry))
+            normDays = days - indexNorm
+            axCountryNorm.semilogy(normDays, countryDict[name], label=name)
 
-        maxNormDays = max(maxNormDays, max(normDays))
-        maxVal = max(maxVal, max(countryDict[name]))
-        
-axCountry.legend(fontsize=legendFontsize)
-axCountry.set_xlabel("Absolute Days")
-axCountry.set_ylabel("Confirmed Cases")
-axCountry.grid(True)
+            maxNormDays = max(maxNormDays, max(normDays))
+            maxVal = max(maxVal, max(countryDict[name]))
+            
+    axCountry.legend(fontsize=legendFontsize)
+    axCountry.set_xlabel("Absolute Days")
+    axCountry.set_ylabel("Confirmed Cases")
+    axCountry.grid(True)
 
-axCountryNorm.legend(fontsize=legendFontsize)
-axCountryNorm.set_xlabel("Normalized Days")
-axCountryNorm.set_ylabel("Confirmed Cases")
+    axCountryNorm.legend(fontsize=legendFontsize)
+    axCountryNorm.set_xlabel("Normalized Days")
+    axCountryNorm.set_ylabel("Confirmed Cases")
 
-axCountryNorm.set_xlim([0, 1.1*maxNormDays])
-axCountryNorm.set_ylim([normValueConfirmedCountry, 1.1*maxVal])
-axCountryNorm.grid(True)
+    axCountryNorm.set_xlim([0, 1.1*maxNormDays])
+    axCountryNorm.set_ylim([normValueConfirmedCountry, 1.1*maxVal])
+    axCountryNorm.grid(True)
 
-figCountry.savefig("confirmed_country.png")
-pl.show()
+    figCountry.savefig("confirmed_country.png")
+
+    if(showPlots):
+        pl.show()
